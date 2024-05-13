d2b3b5d20651af75f33aecc7b66c502abc537dc2
SragZhranOBS <srag.zhran@orange.com>
2024-05-13 13:24:06 +0300

    mme dynamic mnc test

--- src/mme/mme-fd-path.c
+++ src/mme/mme-fd-path.c
@@ -722,14 +722,15 @@ void mme_s6a_send_air(mme_ue_t *mme_ue,
     ogs_assert(ret == 0);
 
     /* Set the Destination-Realm AVP */
-      if(mme_ue->imsi_bcd[OGS_MAX_IMSI_BCD_LEN])
-    {
-        ogs_info("**mme-fd**TRUE__mnc=3**\n");
-    }
-    else {
-        ogs_info("**mme-fd**FALSE__mnc=2**\n");
-    }
-    DiamId_t dest_realm = ogs_set_realm_from_imsi_bcd(mme_ue->imsi_bcd,2);
+    //   if(ogs_plmn_id_mnc_len(&mme_ue->tai.plmn_id)==3)//*****
+    // {
+    //     ogs_info("**mme-fd**TRUE__mnc=3**\n");
+    // }
+    // else {
+    //     ogs_info("**mme-fd**FALSE__mnc=2**\n");
+    // }
+    ogs_info("**mme_s6a_send_air**mnc =  %d*** \n",ogs_plmn_id_mnc_len(&mme_ue->tai.plmn_id));
+    DiamId_t dest_realm = ogs_set_realm_from_imsi_bcd(mme_ue->imsi_bcd,ogs_plmn_id_mnc_len(&mme_ue->tai.plmn_id));
     ret = fd_msg_avp_new(ogs_diam_destination_realm, 0, &avp);
     ogs_assert(ret == 0);
     val.os.data = (unsigned char *)(dest_realm);
@@ -1132,14 +1133,15 @@ void mme_s6a_send_ulr(mme_ue_t *mme_ue)
     ogs_assert(ret == 0);
 
     /* Set the Destination-Realm AVP */
-      if(mme_ue->imsi_bcd[OGS_MAX_IMSI_BCD_LEN])
-    {
-        ogs_info("**mme-fd**TRUE__mnc=3**\n");
-    }
-    else {
-        ogs_info("**mme-fd**FALSE__mnc=2**\n");
-    }
-    DiamId_t dest_realm = ogs_set_realm_from_imsi_bcd(mme_ue->imsi_bcd,2);
+    //   if(mme_ue->imsi_bcd[OGS_MAX_IMSI_BCD_LEN])
+    // {
+    //     ogs_info("**mme-fd**TRUE__mnc=3**\n");
+    // }
+    // else {
+    //     ogs_info("**mme-fd**FALSE__mnc=2**\n");
+    // }
+    ogs_info("**mme_s6a_send_url**mnc =  %d*** \n",ogs_plmn_id_mnc_len(&mme_ue->tai.plmn_id));
+    DiamId_t dest_realm = ogs_set_realm_from_imsi_bcd(mme_ue->imsi_bcd,ogs_plmn_id_mnc_len(&mme_ue->tai.plmn_id));
     ret = fd_msg_avp_new(ogs_diam_destination_realm, 0, &avp);
     ogs_assert(ret == 0);
     val.os.data = (unsigned char *)(dest_realm);
@@ -1297,14 +1299,15 @@ void mme_s6a_send_pur(mme_ue_t *mme_ue)
 
     /* Set the Destination-Realm AVP */
 
-    if(mme_ue->imsi_bcd[OGS_MAX_IMSI_BCD_LEN])
-    {
-        ogs_info("**mme-fd**TRUE__mnc=3**\n");
-    }
-    else {
-        ogs_info("**mme-fd**FALSE__mnc=2**\n");
-    }
-    DiamId_t dest_realm = ogs_set_realm_from_imsi_bcd(mme_ue->imsi_bcd,2);
+    // if(mme_ue->imsi_bcd[OGS_MAX_IMSI_BCD_LEN])
+    // {
+    //     ogs_info("**mme-fd**TRUE__mnc=3**\n");
+    // }
+    // else {
+    //     ogs_info("**mme-fd**FALSE__mnc=2**\n");
+    // }
+    ogs_info("**mme_s6a_send_pur**mnc =  %d*** \n",ogs_plmn_id_mnc_len(&mme_ue->tai.plmn_id));
+    DiamId_t dest_realm = ogs_set_realm_from_imsi_bcd(mme_ue->imsi_bcd,ogs_plmn_id_mnc_len(&mme_ue->tai.plmn_id));
     ret = fd_msg_avp_new(ogs_diam_destination_realm, 0, &avp);
     ogs_assert(ret == 0);
     val.os.data = (unsigned char *)(dest_realm);
